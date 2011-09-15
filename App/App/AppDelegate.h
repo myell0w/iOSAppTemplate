467859576d8601ad4bffa4345a21fbd197afb2b2
Matthias Tretter <matthias.tretter@gmail.com>
2011-09-15 22:43:16 +0200

    Initial Commit

---
+++ App/App/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  App
+//
+//  Created by Tretter Matthias on 15.09.11.
+//  Copyright (c) 2011 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ App/App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  App
+//
+//  Created by Tretter Matthias on 15.09.11.
+//  Copyright (c) 2011 NOUS Wissensmanagement GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
