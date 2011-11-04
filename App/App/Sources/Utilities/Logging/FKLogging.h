6ab08b23d17accc0da9675621ab869bb581ead23
Matthias Tretter <myell0w@me.com>
2011-11-04 22:59:25 +0100

    Improved logging macros

--- App/App/Sources/Utilities/Logging/FKLogging.h
+++ App/App/Sources/Utilities/Logging/FKLogging.h
@@ -1,5 +1,7 @@
 // Part of iOSAppTemplate http://foundationk.it
 
+#import "FKLog.h"
+
 /**
  Enum defining possible log levels
  */
@@ -15,9 +17,9 @@ typedef enum {
 // In Debug-Mode we log everything form LogLevel Info up,
 // else we only log warnings and errors
 #if DEBUG
-    static FKLogLevel kFKLogLevel = FKLogLevelInfo;
+static FKLogLevel kFKLogLevel = FKLogLevelInfo;
 #else
-    static FKLogLevel kFKLogLevel = FKLogLevelWarning;
+static FKLogLevel kFKLogLevel = FKLogLevelWarning;
 #endif
 
 /**
@@ -47,19 +49,24 @@ NS_INLINE NSString* FKLogLevelDescription(FKLogLevel logLevel) {
  This function logs the log message, if the given logLevel is greater or equal
  to the currently defined global logLevel kFKLogLevel
  */
-NS_INLINE void FKLogWithLevel(FKLogLevel logLevel, NSString *logMessage) {
+NS_INLINE void FKLogWithLevel(FKLogLevel logLevel, NSString *file, unsigned int line, NSString *logMessage) {
     if (logLevel >= kFKLogLevel) {
-        NSLog(@"%@ %@", logMessage, FKLogLevelDescription(logLevel));
+        NSLog(@"[%@:%u] %@ %@", file, line, logMessage, FKLogLevelDescription(logLevel));
     }
 }
 
 // Shortcuts for logging with the given log levels
-#define FKLogVerbose(...)       FKLogWithLevel(FKLogLevelVerbose, FKLogToString(__VA_ARGS__))
-#define FKLogInfo(...)          FKLogWithLevel(FKLogLevelInfo, FKLogToString(__VA_ARGS__))
-#define FKLogWarning(...)       FKLogWithLevel(FKLogLevelWarning, FKLogToString(__VA_ARGS__))
-#define FKLogError(...)         FKLogWithLevel(FKLogLevelError, FKLogToString(__VA_ARGS__))
+#define FKLogVerbose(...)       FKLogWithLevel(FKLogLevelVerbose, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#define FKLogInfo(...)          FKLogWithLevel(FKLogLevelInfo, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#define FKLogWarning(...)       FKLogWithLevel(FKLogLevelWarning, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+#define FKLogError(...)         FKLogWithLevel(FKLogLevelError, [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:__VA_ARGS__])
+
+// Logs variables with log level Info
+#define FKLogVariables(...)          do { if (FKLogLevelInfo >= kFKLogLevel) { FKLog(__VA_ARGS__)} } while(0)
+// Logs variables with log level error
+#define FKLogErrorVariables(...)     do { if (FKLogLevelError >= kFKLogLevel) { FKLog(__VA_ARGS__)} } while(0)
 
 // Logs the function/method that got entered
-#define FKLogEntry()            { NSString *enteredFunction = [NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding]; FKLogInfo(enteredFunction); }
+#define FKLogEntry()            do { NSString *enteredFunction = [NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding]; FKLog(enteredFunction); } while(0)
 // Logs that this method is not implemented and aborts
-#define FKLogNotImplemented()   do { FKLogError(@"Not implemented yet."); [self doesNotRecognizeSelector:_cmd];} while(0)
+#define FKLogNotImplemented()   do { NSString *enteredFunction = [NSString stringWithCString:__PRETTY_FUNCTION__ encoding:NSUTF8StringEncoding]; FKLogError(@"Not implemented yet.", enteredFunction); [self doesNotRecognizeSelector:_cmd];} while(0)
